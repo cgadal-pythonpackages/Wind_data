a16134e6509cb16695ead8f3c2b231fd6b5862c3
Cyril Gadal <gadal@ipgp.fr>
2019-05-24 14:31:24 +0200

    Fixing errors in Creating KMZ

--- Era_5.py
+++ Era_5.py
@@ -2,13 +2,13 @@
 # @Date:   2019-05-21T18:44:14+02:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-05-24T14:28:54+02:00
+# @Last modified time: 2019-05-24T14:31:16+02:00
 
 # @Author: gadal
 # @Date:   2018-11-09T14:00:41+01:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-05-24T14:28:54+02:00
+# @Last modified time: 2019-05-24T14:31:16+02:00
 
 import cdsapi
 import os
@@ -181,7 +181,7 @@ class Wind_data:
 
             ##### Wrtiting closure
             with open(os.path.join(loc_path,'bottom_page.kml'),'r') as bottom :
-                dest.write(islice(bottom.read(),7,None))
+                dest.write(bottom.readlines()[7:])
 
 
     def Extract_UV(self, path_to_wgrib = None):
