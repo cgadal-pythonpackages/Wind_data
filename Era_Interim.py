4c544f66007caf507b5f12ba3369c891bda9f26c
Cyril Gadal <gadal@ipgp.fr>
2019-09-23 15:16:30 +0200

    Fixing mistakes on printing the lfux roses

--- Era_Interim.py
+++ Era_Interim.py
@@ -2,7 +2,7 @@
 # @Date:   2018-11-09T14:00:41+01:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-09-23T14:03:13+02:00
+# @Last modified time: 2019-09-23T15:16:13+02:00
 
 from ecmwfapi import ECMWFDataServer
 import os
@@ -246,7 +246,7 @@ class Wind_data:
         for y in range(self.Uwind.shape[1]):
             for x in range(self.Uwind.shape[0]):
                 print('Point number' + str(i))
-                pdfQ, Angle, _  = PDF_flux(self.Qstrength[x,y,:], self.Qorientation[x,y,:])
+                pdfQ, Angle  = PDF_flux(self.Qstrength[x,y,:], self.Qorientation[x,y,:])
                 fig = plt.figure()
                 flux_rose(Angle[:-1],pdfQ, fig = fig, **kwargs)
                 plt.savefig(dir + '/flux_rose_'+ format_string.format(i+1) + ext)
