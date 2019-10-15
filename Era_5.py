Fixing lat lon calculation

Era_5.py
@@ -2,13 +2,13 @@
 # @Date:   2019-05-21T18:44:14+02:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-10-15T17:29:35+02:00
+# @Last modified time: 2019-10-15T17:31:21+02:00
 
 # @Author: gadal
 # @Date:   2018-11-09T14:00:41+01:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-10-15T17:29:35+02:00
+# @Last modified time: 2019-10-15T17:31:21+02:00
 
 import cdsapi
 import os
@@ -90,7 +90,7 @@ class Wind_data:
         variable_dic['area'] = area_wanted
         self.grid_bounds = area_wanted
         self.lat = np.linspace(self.grid_bounds[0], self.grid_bounds[2], abs(self.grid_bounds[0] - self.grid_bounds[2])/self.grid + 1)
-        self.lon = np.linspace(self.grid_bounds[1], self.grid_bounds[3], abs(self.grid_bounds[0] - self.grid_bounds[2])/self.grid + 1)
+        self.lon = np.linspace(self.grid_bounds[1], self.grid_bounds[3], abs(self.grid_bounds[1] - self.grid_bounds[3])/self.grid + 1)
         print('Area is :', area_wanted)
         # print('Please ensure that the area returned by ECMWF correspond to this Area. Otherwise correct it by modifying self.area afterwards.')
 
@@ -139,7 +139,7 @@ class Wind_data:
         self.years = dict_from_file['years']
         self.grid = dict_from_file['grid']
         self.lat = np.linspace(self.grid_bounds[0], self.grid_bounds[2], abs(self.grid_bounds[0] - self.grid_bounds[2])/self.grid + 1)
-        self.lon = np.linspace(self.grid_bounds[1], self.grid_bounds[3], abs(self.grid_bounds[0] - self.grid_bounds[2])/self.grid + 1)
+        self.lon = np.linspace(self.grid_bounds[1], self.grid_bounds[3], abs(self.grid_bounds[1] - self.grid_bounds[3])/self.grid + 1)
 
     def Extract_UV(self, path_to_wgrib = None):
         if path_to_wgrib != None: