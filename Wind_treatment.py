8f7a660fc4681e1e1704653360a2841a8c1b2f78
Cyril Gadal <gadal@ipgp.fr>
2019-10-23 13:36:59 +0200

    Calculation of distribution using numpy

--- Wind_treatment.py
+++ Wind_treatment.py
@@ -2,7 +2,7 @@
 # @Date:   2018-12-11T14:18:01+01:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-09-23T19:16:59+02:00
+# @Last modified time: 2019-10-23T13:34:32+02:00
 
 
 
@@ -70,7 +70,7 @@ def flux_rose(Angle, PdfQ_tp, withaxe = 0, place = None, fig = None, color = 'gr
         if withaxe != 1:
             ax.set_yticks([])
 
-def Wind_to_flux(wind_direction, wind_strength, grain_size, z_0 = 1e-3, z = 10, rhoair = 1.293, rhosed = 2.55e3):
+def Wind_to_flux(wind_direction, wind_strength, grain_size, z_0 = 1e-3, z = 10, rhoair = 1.293, rhosed = 2.55e3, threshold = False):
         # Data : first column are directions, second column are speed
         # d : grain diameter
         # dt : step of the time serie
@@ -90,28 +90,31 @@ def Wind_to_flux(wind_direction, wind_strength, grain_size, z_0 = 1e-3, z = 10, 
 
         ######## flux
         # qs = np.maximum(0,((ut*rhoair)/(rhosed *grain_size))*(u**2-ut**2))
-        qs = np.maximum(0,(25*(rhoair/rhosed)*np.sqrt(grain_size/g))*(u**2-ut**2))
-        return qs, direction
+        if not threshold :
+            qs = np.maximum(0,(25*(rhoair/rhosed)*np.sqrt(grain_size/g))*(u**2-ut**2))
+            return qs, direction
+        else:
+            qs = np.maximum(0,(25*(rhoair/rhosed)*np.sqrt(grain_size/g))*(u**2-ut**2))
+            r = np.maximum(1, (25*(rhoair/rhosed)*np.sqrt(grain_size/g))*(u**2-ut**2))
 
 
 def PDF_flux(direction, qs):
+    # direction : flux direction
+    # qs : flux intensity
+     return Make_angular_PDF(direction, qs)
 
-    # Data : first column are directions, second column are speed
-    # d : grain diameter
-    # dt : step of the time serie
-    # z_0 = 1e-3 : Roughness of the sediment layer. (meters)
-    # z = 10  : Height of wind speed measurment.
-    # rhoair = 1.293 : air density [kg/m^3].
-    # rhosed = 2.55e3 : sediment density [kg/m^3].
 
-    Ntheta = 360
-    dN = 360/(Ntheta-1)
-    pdfQ = np.zeros((Ntheta-1,))
+def Make_angular_PDF(quantity, weight):
+    hist, bin_edges = np.histogram(quantity, bins = np.linspace(0, 360, 361), density = 1, weights = weight)
+    a = np.array([np.mean(bin_edges[i:i+2]) for i in range(bin_edges.size -1)])
+    return hist, a
 
-    Angle = np.linspace(0,Ntheta,360)
-    for n in range(Ntheta-1):
-        ind = (direction >= Angle[n]) & (direction < Angle[n+1])
-        pdfQ[n] = np.nansum(qs[ind])
 
-    Angle = Angle + ((Angle[1]-Angle[0])/2)%360
-    return pdfQ/pdfQ.sum(), Angle[:-1]
+def Make_threshold_distribution(direction, r):
+    bin_edges = np.linspace(0, 360, 361)
+    ut = []
+    for i in range(bin_edges.size - 1):
+        vel = r[(direction >= bin_edges[i]) & (direction > bin_edges[i+1])]
+        ut.append(np.mean(vel[vel > 1]))
+    a = np.array([np.mean(bin_edges[i:i+2]) for i in range(bin_edges.size -1)])
+    return np.array(ut), a
