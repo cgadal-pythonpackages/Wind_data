bf1f91e3301ecd0848a10f008086a0c0ff9767dd
Cyril Gadal <gadal@ipgp.fr>
2019-10-23 13:52:34 +0200

    Fixing Wind to flux calculation of distance to threshold

--- Wind_treatment.py
+++ Wind_treatment.py
@@ -2,7 +2,7 @@
 # @Date:   2018-12-11T14:18:01+01:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2019-10-23T13:49:34+02:00
+# @Last modified time: 2019-10-23T13:52:32+02:00
 
 
 
@@ -119,6 +119,6 @@ def Make_threshold_distribution(direction, r):
         if vel[vel >= 1].size == 0:
             ut.append(1)
         else:
-            ut.append(np.mean(vel[vel > 1]))
+            ut.append(np.mean(vel[vel >= 1]))
     a = np.array([np.mean(bin_edges[i:i+2]) for i in range(bin_edges.size -1)])
     return np.array(ut), a
