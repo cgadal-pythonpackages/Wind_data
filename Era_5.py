a5770eab418b15c41c9b29fdf370483e3a588283
Cyril Gadal <gadal@ipgp.fr>
2020-11-02 17:53:23 +0100

    Fixing errors

--- Era_5.py
+++ Era_5.py
@@ -2,7 +2,7 @@
 # @Date:   2019-05-21T18:44:14+02:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2020-11-02T17:52:38+01:00
+# @Last modified time: 2020-11-02T17:53:14+01:00
 
 import cdsapi
 import os
@@ -136,7 +136,7 @@ class Wind_data:
             self.time.append(file_temp.variables['time'][:])
             if i == 0:
                 self.latitude = file_temp.variables['latitude'][:]
-                self.longitudes = file_temp.variables['longitude'][:]
+                self.longitude = file_temp.variables['longitude'][:]
 
         self.Uwind, self.Vwind, self.time = np.concatenate(self.Uwind, axis = -1), np.concatenate(self.Uwind, axis = -1), np.concatenate(self.time, axis = -1)
         self.Save_basic()
