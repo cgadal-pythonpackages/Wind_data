9874f2891210534707036d2a3063bcec6b2ad8c8
Cyril Gadal <gadal@ipgp.fr>
2021-03-09 01:18:50 +0100

    removing save infox from downloading function

--- Wind_data/Era_5.py
+++ Wind_data/Era_5.py
@@ -2,7 +2,7 @@
 # @Date:   2019-05-21T18:44:14+02:00
 # @Email:  gadal@ipgp.fr
 # @Last modified by:   gadal
-# @Last modified time: 2021-03-08T22:23:27+01:00
+# @Last modified time: 2021-03-09T01:18:28+01:00
 
 import cdsapi
 import os
@@ -150,8 +150,8 @@ class Wind_data:
     def Save_spec_to_txt(self, name):
         Pars_to_save = ['name', 'type', 'years', 'file_names']
         dic_attr = { i: getattr(self, i) for i in Pars_to_save}
-        dic_attr['latitude'] = list(dic_attr['latitude'])
-        dic_attr['longitude'] = list(dic_attr['longitude'])
+        # dic_attr['latitude'] = list(dic_attr['latitude'])
+        # dic_attr['longitude'] = list(dic_attr['longitude'])
         if os.path.isfile(name):
             print(name + ' already exists')
         else:
